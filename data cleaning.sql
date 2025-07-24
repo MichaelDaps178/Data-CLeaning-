c61dcd739f0b0ae94847c368c69f3195c3dbce9a
Michael Opoku Dapaah <MichaelDaps178@gmail.com>
2025-07-24 17:39:28 -0400

    Update data cleaning.sql

--- data cleaning.sql
+++ data cleaning.sql
@@ -55,12 +55,107 @@ PARTITION BY company, location,  industry, total_laid_off, percentage_laid_off, 
 FROM layoffs_staging;
 
 SELECT *
-FROM layoffs_staging2
-WHERE row_num > 1;
+FROM layoffs_staging2;
 
 DELETE
 FROM layoffs_staging2
 WHERE row_num > 1;
+
+
+-- Standardizing Data (Finding issues in Data and Fixing It)
+
+SELECT company, (TRIM(company))
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = (TRIM(company));
+
+SELECT industry
+FROM layoffs_staging2;
+
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+WHERE country LIKE 'United States%'
+ORDER BY 1;
+
+SELECT DISTINCT country, TRIM(TRAILING '.'FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT DISTINCT country,  TRIM(TRAILING '.'FROM country)
+FROM layoffs_staging2
+WHERE country LIKE 'United States%';
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.'FROM country)
+WHERE country LIKE 'United States%';
+
+SELECT `date`
+FROM layoffs_staging2;
+
+SELECT `date`,
+STR_TO_DATE(`date`, '%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+SELECT * FROM
+layoffs_staging2
+WHERE total_laid_off is NULL
+AND percentage_laid_off IS NULL;
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL;
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL)
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
 
 
 
